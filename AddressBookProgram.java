ab2a9153c68bcb80de105a98c7de66698189c8b9
Rekha-15 <rekhapatil.1509@gmail.com>
2021-06-08 20:57:54 -0400

    add : AddressBookProgram.java

--- AddressBookProgram.java
+++ AddressBookProgram.java
@@ -1,25 +1,18 @@
 package com;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-
-/**
- * @author rekha
- * Person is a class of public type
- * Add method to take an input from user to add a contact details to address book
- * created an object of  Person class has Person1
- *implementing from IAddressBook interface
- */
-
-
+import java.util.stream.Collectors;
 
 public class AddressBookProgram implements IAddressBook
 {
 	
 	Scanner scanner = new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 	List<Person> personList = new ArrayList<Person>();
 	
-	public void add()
+	public void add() 
 	{
 	    System.out.println("Enter your first name");
 	    String firstName = scanner.nextLine();
@@ -41,11 +34,7 @@ public class AddressBookProgram implements IAddressBook
 	    System.out.println("Contact added successfully");
          }
 	
-	/**
-	 * Creating display method to display the contact details 
-	 */
-	
-	 public void display()
+	 public void display() 
 	 {
 	      for(int i = 0; i< personList.size(); i++)
 	      {
@@ -55,15 +44,9 @@ public class AddressBookProgram implements IAddressBook
 	    	}
 	 }
 	
-	 /**
-	  * creating the method to edit the contact details already given to address book
-	  * User wants to give there name and can start editing
-	  *
-	  */ 
-	 
-	 public void edit(String firstName) 
+	 public void edit(String firstName)
 	 {
-             for (int i = 0; i < personList.size(); i++) 
+             for (int i = 0; i < personList.size(); i++)
              {
             	Person person = personList.get(i);
             	Scanner scanner = new Scanner(System.in);
@@ -92,65 +75,59 @@ public class AddressBookProgram implements IAddressBook
           }
             
         }
-	 
-	 /**
-	  * 
-	  *Creating delet method , so that user can delete the contact details which is not required for user
-	  *by entering first name user can delet the contact
-	  */
-	 
-	 public void delete(String name)
+	
+	 public void delete(String name) 
 	 {
-         for (int i = 0; i < personList.size(); i++) 
-         {
-         	if(personList.get(i).getFirstName().equals(name))
-         	{
-         		Person person = personList.get(i);
-         		personList.remove(person);
-         	}
+            for (int i = 0; i < personList.size(); i++) 
+            {
+            	if(personList.get(i).getFirstName().equals(name)) 
+            	{
+            		Person person = personList.get(i);
+            		personList.remove(person);
+            	}
+            }
          }
-      }
+
+	public void addMultiplePerson() 
+	{
+        	 
+             System.out.println("Enter a person Name:");
+             String firstName = sc.nextLine();
+             for(int i = 0; i <  personList.size(); i++) 
+             {
+        	  Person person = personList.get(i);
+		}
+	}
 	   
-	 /**
-	  * Printing the welcome message
-	  * AddressBookProgram is an class 
-	  * created an object of this AddressBookProgram class has AddressBookProgram1
-	  * if condition is true
-	  * By using switchCase asking user to what task user needs to perform 
-	  * option will be given to user to select an number
-	  * 1= adding contact details
-	  * 2=displaying contact details
-	  *3=editing contact details
-	  *4=deleting details
-	  */
-	 
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to Address Book Program");
 		 
-		AddressBookProgram AddressBookProgram1 =new AddressBookProgram();
+		AddressBookProgram adressBookImplementation =new AddressBookProgram();
 	        boolean condition = true;
 	     
 	        while (condition == true) 
 	        {
 	    	     Scanner scanner= new Scanner(System.in);
 	    	     Scanner option = new Scanner(System.in);
-	             System.out.println("What you want to perform ?"+"\n"+"1.Add Person"+"\n"+"2.Display"+"\n"+"3.Edit person"+"\n"+"4."+"Delete Person");
+	             System.out.println("What you want to perform ?"+"\n"+"1.Add Person"+"\n"+"2.Display"+"\n"+"3.Edit person"+"\n"+"4."+"Delete Person"+"\n"+"5.Add Multiple Person");
 	         
-                    switch(option.nextInt()) 
-                    {
-	                 case 1:AddressBookProgram1.add();
+                     switch(option.nextInt()) 
+                     {
+	                 case 1:adressBookImplementation.add();
 	                       break;
-	             	 case 2:AddressBookProgram1.display(); 
-                              break;
+	             	 case 2:adressBookImplementation.display(); 
+                               break;
 	                 case 3:System.out.println("Enter the firstName:");
 	                       String firstName = scanner.nextLine();
-	                       AddressBookProgram1.edit(firstName);
+	            	       adressBookImplementation.edit(firstName);
 	            	       break;
 			 case 4:System.out.println("Enter the Name of the person do you wants to delete");
 	                       String name = scanner.nextLine();
-	                       AddressBookProgram1.delete(name);
+	            	       adressBookImplementation.delete(name);
 	            	       break;
+			 case 5:adressBookImplementation.addMultiplePerson();
+	                       break;
 			default:System.out.println("Thank You");
 			}
 		}
